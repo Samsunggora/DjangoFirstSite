bb485e0242d4312a5e72176368442823d5efbc8b
Jorge Likhanov <samsunggora@gmail.com>
2021-07-19 16:38:51 +0200

    add def function on urls and views

--- testStite/settings.py
+++ testStite/settings.py
@@ -109,3 +109,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# 6
--- testStite/urls.py
+++ testStite/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from news.views import index, test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('news/', index),
+    path('test/', test),
+
 ]
