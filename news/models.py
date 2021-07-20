078783b61e7892bed5cd3d5b848290ba3d8289bf
Jorge Likhanov <samsunggora@gmail.com>
2021-07-20 19:46:25 +0200

    add verbosenam

--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Созданно')
     update_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Выложенно? ')
 
     def __str__(self):
