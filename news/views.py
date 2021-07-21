8061d3f82a308c5a8f7840770c119cb0566ac6d7
Jorge Likhanov <samsunggora@gmail.com>
2021-07-21 21:42:03 +0200

    add urls

--- news/views.py
+++ news/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'news list'})
+    categories = Category.objects.all()
+    return render(request, 'news/index.html', {'news': news, 'title': 'news list', 'category': categories, })
+
+
+def  get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'title': 'news list', 'category': categories, })
