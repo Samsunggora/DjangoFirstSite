0bc02faa2c50fb0babc8a1323abcef6cbb200d9c
Jorge Likhanov <samsunggora@gmail.com>
2021-07-23 15:08:45 +0200

    add urls

--- news/views.py
+++ news/views.py
@@ -5,12 +5,10 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'news list', 'category': categories, })
+    return render(request, 'news/index.html', {'news': news, 'title': 'news list', })
 
 
-def  get_category(request, category_id):
+def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'title': 'news list', 'category': categories, })
+    return render(request, 'news/category.html', {'news': news, 'title': 'news list', })
