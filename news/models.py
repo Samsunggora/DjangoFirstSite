9ed37624a7b29f074e547a2bba49d39505f6ce40
Jorge Likhanov <samsunggora@gmail.com>
2021-07-24 20:42:54 +0200

    add get_absolute_url and add new urls

--- news/models.py
+++ news/models.py
@@ -26,6 +26,9 @@ class News(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Выложенно?')
     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
+    def get_absolute_url(self):
+        return reverse('get_view', kwargs={'news_id': self.pk})
+
     def __str__(self):
         return self.title
 
--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import index, get_category
+from .views import index, get_category, get_view
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<int:category_id>/', get_category, name='category')
+    path('category/<int:category_id>/', get_category, name='category'),
+    path('new/<int:news_id>', get_view, name='get_view'),
 
 ]
