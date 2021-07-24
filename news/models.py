f97320f2171681d22c0800dbc418cbfebeea7960
Jorge Likhanov <samsunggora@gmail.com>
2021-07-24 20:25:39 +0200

    add get_absolute_url and add new urls

--- news/models.py
+++ news/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'category_id': self.pk})
 
     def __str__(self):
         return self.title
