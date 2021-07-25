4d305eceae70d2d7713abeafabfe5767513643fa
Jorge Likhanov <samsunggora@gmail.com>
2021-07-25 16:41:34 +0200

    add post form code and html file

--- news/forms.py
+++ news/forms.py
@@ -3,7 +3,11 @@ from .models import Category
 
 
 class New_forms(forms.Form):
-    title = forms.CharField(max_length=255, min_length=3)
-    content = forms.CharField()
-    is_published = forms.BooleanField
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=255, min_length=3, label='Title',
+                            widget=forms.TextInput(attrs={'class': "form-control"}))
+    content = forms.CharField(label='Content:', required=False,
+                              widget=forms.Textarea(attrs={'class': "form-control"}))
+    is_published = forms.BooleanField(label='Published?', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Category:',
+                                      empty_label='Please choose category',
+                                      widget=forms.Select(attrs={'class': "form-control"}))
